fc2f06d9e9d66faff8dc7c81d83093e481ee5b43
James Ritchie <james.g.ritchie@baesystems.com>
2019-03-21 11:11:38 +0000

    working redaction pipeline

--- main.go
+++ main.go
@@ -10,24 +10,19 @@ import (
 	"log"
 	"time"
 
-	// "github.com/globalsign/mgo"
-	// "gopkg.in/mgo.v2/bson"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Person struct {
-	Name  string
-	Grade string
-}
-
 var (
-	port     int
-	rootPath string
-	certPath string
-	keyPath  string
+	port          int
+	rootPath      string
+	certPath      string
+	keyPath       string
+	ctx, _        = context.WithTimeout(context.Background(), 8*time.Second)
+	client        *mongo.Client
+	resetDatabase bool
 )
 
 func getFlags() {
@@ -35,55 +30,130 @@ func getFlags() {
 	flag.StringVar(&rootPath, "root", "", "path to root certificate")
 	flag.StringVar(&certPath, "cert", "", "path to client certificate")
 	flag.StringVar(&keyPath, "key", "", "path to client key")
+	flag.BoolVar(&resetDatabase, "r", false, "Reset the test data")
 	flag.Parse()
 }
 
 func main() {
 
 	getFlags()
-
 	url := fmt.Sprintf("mongodb://localhost:%v", port)
-	log.Println(url)
+
+	client = getMongoClient(url)
+	coll := client.Database("test").Collection("people")
+
+	if resetDatabase {
+		coll.DeleteMany(ctx, bson.M{})
+	}
+
+	if isCollectionEmpty(coll) {
+		log.Println("Populating dummy data")
+		insertDummyData(coll)
+	}
+
+	userTags2 := bson.A{"HR", "IT", "MAN"}
+	query := bson.M{"contact info.email": "james@bae.com"}
+
+	mongopl := mongo.Pipeline{
+		bson.D{
+			{"$match", query}},
+		bson.D{
+			{"$redact", bson.M{
+				"$cond": bson.M{
+					"if":   bson.M{"$gt": bson.A{bson.M{"$size": bson.M{"$setIntersection": bson.A{"$tags", userTags2}}}, 0}},
+					"then": "$$DESCEND",
+					"else": "$$PRUNE"}}}},
+	}
+
+	cur, err := coll.Aggregate(ctx, mongopl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+	printResults(cur)
+}
+
+func insertDummyData(coll *mongo.Collection) {
+	record := []interface{}{
+		bson.D{
+			{"name", "Pritam"},
+			{"tags", bson.A{"HR", "IT"}},
+			{"contact info", bson.A{
+				bson.D{
+					{"phone", "01234 567890"},
+					{"tags", bson.A{"MAN", "HR"}},
+				},
+				bson.D{
+					{"email", "pritam@bae.com"},
+					{"tags", bson.A{"IT"}},
+				}}}},
+		bson.D{
+			{"name", "James"},
+			{"tags", bson.A{"HR", "IT"}},
+			{"contact info", bson.A{
+				bson.D{
+					{"phone", "01234 567890"},
+					{"tags", bson.A{"HR", "MAN"}},
+				},
+				bson.D{
+					{"email", "james@bae.com"},
+					{"tags", bson.A{"IT"}}}}}},
+		bson.D{
+			{"name", "Russ"},
+			{"tags", bson.A{"HR", "IT"}},
+			{"contact info", bson.A{
+				bson.D{
+					{"phone", "01234 567890"},
+					{"tags", bson.A{"HR", "MAN"}},
+				},
+				bson.D{
+					{"email", "russell@bae.com"},
+					{"tags", bson.A{"IT", "MAN"}},
+				}}}},
+	}
+	coll.InsertMany(ctx, record, nil)
+}
+
+func isCollectionEmpty(coll *mongo.Collection) bool {
+	docCount, err := coll.CountDocuments(ctx, bson.M{}, nil)
+	if err != nil {
+		log.Println(err)
+		return true
+	}
+	return docCount == 0
+}
+
+func printResults(cur *mongo.Cursor) {
+	noRecords := true
+	for cur.Next(ctx) {
+		noRecords = false
+		var result bson.D
+		err := cur.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%v\n", result)
+	}
+	if noRecords {
+		fmt.Println("No matches.")
+	}
+}
+
+func getMongoClient(url string) *mongo.Client {
 	opt := options.Client().ApplyURI(url)
 	if len(rootPath) > 0 {
 		tlsConfig := getTLSConfig()
 		opt.TLSConfig = tlsConfig
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 8*time.Second)
 	client, err := mongo.Connect(ctx, opt)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// if err = client.Ping(ctx, nil); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	coll := client.Database("test").Collection("people")
-
-	// record := &Person{Name: "Bob", Grade: "T3"}
-
-	// coll.InsertOne(ctx, record)
-
-	// var records []struct {
-	// 	Name  string
-	// 	Grade string
-	// }
-	log.Println("Querying...")
-
-	cur, err := coll.Find(ctx, bson.M{"name": "Bob"})
-	if err != nil {
-		log.Fatal("record not found")
+	if err = client.Ping(ctx, nil); err != nil {
+		log.Fatal(err)
 	}
-	defer cur.Close(ctx)
-	for cur.Next(ctx) {
-		var result bson.M
-		err := cur.Decode(&result)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(result)
-	}
+	return client
 }
 
 func getTLSConfig() *tls.Config {
